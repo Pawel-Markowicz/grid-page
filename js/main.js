4f50fccc591510f1368a6a2d751a782e998fe65e
Pawel Markowicz <pawel.markowicz88@gmail.com>
2022-11-01 16:12:32 +0100

    finishing menu with JS

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 const navMobile = document.querySelector ('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
+const allNavItems = document.querySelectorAll('.nav__item');
 
 // ANIMOWANY HAMBURGER ORAZ WSUWA SIĘ MOBILNA NAWIGACJA
 const handleNav = () => {
-    navBtn.classList.toggle('is-active');
+    // navBtn.classList.toggle('is-active');
     navMobile.classList.toggle('nav-mobile--active');
+
+    allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+            navMobile.classList.remove('nav-mobile--active')
+        })
+    })
+
+
 }
 
 navBtn.addEventListener('click', handleNav);
